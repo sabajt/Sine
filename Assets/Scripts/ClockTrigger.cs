063daf535f367d8682a017092cf4909114bb6c98
John Saba <john.thomas.saba@gmail.com>
2017-01-29 23:24:13 -0600

    clean up clock trigger

--- Assets/Scripts/ClockTrigger.cs
+++ Assets/Scripts/ClockTrigger.cs
@@ -5,85 +5,49 @@ using UnityEngine;
 
 public class ClockTrigger : MonoBehaviour {
 
-	// Generic
-	public GameObject anchor;
-	public GameObject canvas;
-
-	private CameraScript cameraScript;
-	private EventCanvasScript canvasScript;
-	private bool playerInsideTarget = false;
-	private bool isViewingTarget = false;
-
-	// Clock
 	public GameObject tube;
 
 	private bool collectedTube = false;
 	private Vector3 tubePos;
+	private InspectionTrigger inspectionTrigger;
 
-	// Use this for initialization
 	void Start () {
-		// Generic
-		canvas.SetActive (false);
-		canvasScript = canvas.GetComponent<EventCanvasScript> ();
-		canvasScript.hideDetailText ();
-		cameraScript = GameObject.Find ("CameraController").GetComponent<CameraScript> ();
-
-		// clock
 		Vector3 pos = tube.transform.position;
 		tubePos = new Vector3 (pos.x, pos.y, pos.z - 0.2f);
 	}
 
-	void zoomOut() {
-		isViewingTarget = false;
-		cameraScript.UnfocusTarget ();
-		canvasScript.hideDetailText ();
+	void OnEnable () {
+		inspectionTrigger = gameObject.GetComponent<InspectionTrigger> ();
+
+		inspectionTrigger.OnStartViewTarget += OnStartViewTarget;
+		inspectionTrigger.OnEndViewTarget += OnEndViewTarget;
+		inspectionTrigger.OnUpdateViewingTarget += OnUpdateViewingTarget;
 	}
 
-	// Update is called once per frame
-	void Update () {
+	void OnDisable() {
+		inspectionTrigger.OnStartViewTarget -= OnStartViewTarget;
+		inspectionTrigger.OnEndViewTarget -= OnEndViewTarget;
+		inspectionTrigger.OnUpdateViewingTarget -= OnUpdateViewingTarget;
+	}
 
-		if (playerInsideTarget == true) {
+	void OnStartViewTarget () {
 
-			// Generic Event Code
-			if (Input.GetKeyDown (KeyCode.E)) {
-				if (isViewingTarget == false) {
-					isViewingTarget = true;
+	}
 
-					cameraScript.FocusTarget();
-					canvasScript.showDetailText ();
+	void OnEndViewTarget () {
 
-				} else {
-					isViewingTarget = false;
-					cameraScript.UnfocusTarget ();
-					canvasScript.hideDetailText ();
-				}
+	}
+
+	void OnUpdateViewingTarget () {
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			if (collectedTube) {
+				inspectionTrigger.EndViewingTarget ();
 			}
-
-			if (isViewingTarget) {
-				cameraScript.LerpTarget (anchor.transform.position, anchor.transform.rotation, 3.2f);
-
-				// Clock
-				if (Input.GetKeyDown (KeyCode.Return)) {
-					if (collectedTube) {
-						zoomOut ();
-					}
-					collectedTube = true;
-				}
-
-				if (collectedTube) {
-					tube.transform.position = Vector3.Lerp (tube.transform.position, tubePos, Time.deltaTime * 3.0f);
-				}
-			}
+			collectedTube = true;
 		}
-	}
 
-	void OnTriggerEnter(Collider other) {
-		playerInsideTarget = other.gameObject.name == "FPSController";
-		canvas.SetActive(playerInsideTarget);
-	}
-
-	void OnTriggerExit( Collider other ){
-		playerInsideTarget = other.gameObject.name != "FPSController";
-		canvas.SetActive(playerInsideTarget);
+		if (collectedTube) {
+			tube.transform.position = Vector3.Lerp (tube.transform.position, tubePos, Time.deltaTime * 3.0f);
+		}
 	}
 }
